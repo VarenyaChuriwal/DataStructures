08a5eabadc8157a960f9d734f4c6f710a7f1d87a
VarenyaChuriwal <varenyachuriwal@gmail.com>
2022-11-02 18:05:39 +0530

    Implemented Insertion Sort on array

---
+++ insertionSort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int values[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf(i? ", %i": "%i", values[i]);
+    }
+    printf("\n");
+}
+
+void sortArray(int values[], int n) {
+    int i, tmp, currentIndex, j;
+    for (i = 1; i<n; i++) {
+        currentIndex = i;
+        for (j = i-1; j>=0;j--){
+            if (values[j] <= values[currentIndex])
+                break;
+            tmp = values[currentIndex];
+            values[currentIndex] = values[j];
+            values[j] = tmp;
+            currentIndex--;
+        }
+    }
+}
+
+void main() {
+    // Accept array as user input
+    printf("Enter length of array list\n");
+    int n;
+    scanf("%i",&n);
+    int values[n];
+    for (int i=0;i<n;i++) {
+        // printf("Value:");
+        // scanf("%i",&values[i]);
+        values[i] = rand();
+    }
+
+    // Print unsorted array
+    printf("Unsorted Array = ");
+    printArray(values, n);
+    // Sort Array
+    sortArray(values, n);
+    // Print sorted array
+    printf("Sorted Array = ");
+    printArray(values, n);
+}
